62db2db58aa17d6823ffe1a07682a73ccfaabadc
KeMeNoToRiKo <ombrogxavieremmanuel@gmail.com>
2025-05-26 20:28:42 +0800

    login

--- pages/loginPage.php
+++ pages/loginPage.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/registration.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body>
@@ -26,31 +26,25 @@
 
     <section>
         <div class="login-container">
-            <h5 class="mb-4 text-white">Create your account.</h5>
+            <h5 class="login-text mb-4 text-center text-white">Login</h5>
             <form action="register_process.php" method="post" id="registrationForm">
-                <div class="row mb-3">
-                    <div class="col">
-                        <input type="text" name="first_name" class="form-control" placeholder="First name" required>
-                    </div>
-                    <div class="col">
-                        <input type="text" name="last_name" class="form-control" placeholder="Last name" required>
-                    </div>
-                </div>
                 <div class="mb-3">
                     <input type="email" name="email" class="form-control" placeholder="Email" required>
                 </div>
                 <div class="mb-3">
                     <input type="password" name="password" class="form-control" placeholder="Password" required>
                 </div>
-                <div class="mb-3">
-                    <input type="password" name="confirm_password" class="form-control" placeholder="Confirm Password" required>
-                </div>
                 <div class="text-center mt-4">
-                    <button type="submit" class="btn btn-custom"><u>create</u></button>
+                    <button type="submit" class="btn btn-custom"><u>login</u></button>
                 </div>
             </form>
+            <!-- Register link below the login button -->
+            <div class="text-center mt-3">
+                <p class="text-white">Don't have an account? <a href="registrationPage.php" class="text-decoration-underline text-white">Register here</a></p>
+            </div>
         </div>
     </section>
+
     <script src="../js/registration.js"></script>
 </body>
 </html>
